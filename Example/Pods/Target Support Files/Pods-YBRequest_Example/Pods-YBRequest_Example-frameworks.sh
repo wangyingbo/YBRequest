7e80e951db140fffeb35f556941a0d4c168c80fb
wangyingbo <wangyingbo528@163.com>
2020-07-03 13:11:18 +0800

    dependency version

--- Example/Pods/Target Support Files/Pods-YBRequest_Example/Pods-YBRequest_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-YBRequest_Example/Pods-YBRequest_Example-frameworks.sh
@@ -198,10 +198,12 @@ fi
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Aspect/Aspect.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/YBRequest/YBRequest.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Aspect/Aspect.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/YBRequest/YBRequest.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
